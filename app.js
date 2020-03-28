10aa4cde6012ca20acb5715465cfeb9cf72614a3
Nirmal Vettiankal <nirmal.vettiankal@gmail.com>
2020-03-28 17:18:55 -0400

    mongo connection

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ const port = process.env.PORT;
 
 const bodyParser = require('body-parser');
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true
+});
 
 require('./auth.js')(app);
 
