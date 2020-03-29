385669820dc1b29a79c79d2275dbcf9636a4e2f8
nirmalvett <nirmalvettiankal@gmail.com>
2020-03-29 04:33:14 -0400

    use jwt for auth

--- auth.js
+++ auth.js
@@ -1,24 +1,26 @@
 module.exports = (app) => {
 
-    const { auth, requiresAuth } = require("express-openid-connect");
+    const jwt = require('express-jwt');
+    const jwksRsa = require('jwks-rsa');
+    const checkJwt = jwt({
+        secret: jwksRsa.expressJwtSecret({
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 5,
+            jwksUri: process.env.JWT_URI
+        }),
 
-    const config = {
-        required: false,
-        auth0Logout: true,
-        baseURL: process.env.BASE_URL,
-        issuerBaseURL: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENT_ID,
-        appSessionSecret: process.env.SESSION_SECRET
-    };
-
-    app.use(auth(config));
-
-    app.get("/", (req, res) => {
-        res.send(req.isAuthenticated() ? "Logged in" : "Logged out");
+        audience: process.env.AUTH0_CLIENT_ID,
+        issuer: process.env.JWT_ISSUER,
+        algorithms: ['RS256']
     });
 
-    app.get('/profile', requiresAuth(), (req, res) => {
-        res.send(JSON.stringify(req.openid.user));
+    app.get("/", checkJwt, (req, res) => {
+        res.status(200).send();
+    });
+
+    app.get('/profile', checkJwt, (req, res) => {
+        res.send(JSON.stringify(req.user));
     });
 
 }
